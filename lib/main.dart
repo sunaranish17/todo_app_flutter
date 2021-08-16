d1f329a7579fd6ba1eb128115706536ed90ccd52
Anish <sunaranish17@gmail.com>
2021-08-16 14:17:56 +0545

    Inital commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text(
+              "Hello World!!",
+              style: TextStyle(fontSize: 22),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
