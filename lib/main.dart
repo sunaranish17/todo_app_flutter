4358fe9a42fb5d3e4cb49e865febb1d01ecb7949
Anish <sunaranish17@gmail.com>
2021-08-16 14:19:39 +0545

    Feat: Google fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme:
+              GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme)),
       home: Scaffold(
         body: Container(
           child: Center(
