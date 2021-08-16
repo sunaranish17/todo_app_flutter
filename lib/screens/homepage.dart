23a35370f66f76efb5bdb4ed348e1c0a55f3f1cd
Anish <sunaranish17@gmail.com>
2021-08-16 15:54:24 +0545

    Feat: Task page

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:what_todo/screens/taskpage.dart';
 import 'package:what_todo/widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,7 +16,6 @@ class _HomePageState extends State<HomePage> {
           width: double.infinity,
           padding: EdgeInsets.symmetric(
             horizontal: 24.0,
-            vertical: 32.0,
           ),
           color: Color(0xFFF6F6F6),
           child: Stack(
@@ -24,35 +24,54 @@ class _HomePageState extends State<HomePage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: EdgeInsets.only(
-                      bottom: 32.0,
-                    ),
+                    margin: EdgeInsets.only(bottom: 32.0, top: 32.0),
                     child: Image(
                       image: AssetImage('assets/images/logo.png'),
                     ),
                   ),
-                  TaskCardWidget(
-                      title: "Get Started",
-                      desc:
-                          "Hello User! Welcome to TODO app, this is a default task that you can edit or delete to start using the app."),
-                  TaskCardWidget(),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        TaskCardWidget(
+                            title: "Get Started",
+                            desc:
+                                "Hello User! Welcome to TODO app, this is a default task that you can edit or delete to start using the app."),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                      ],
+                    ),
+                  )
                 ],
               ),
               Positioned(
-                bottom: 0.0,
+                bottom: 24.0,
                 right: 0.0,
-                child: Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF7349FE),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Image(
-                    image: AssetImage('assets/images/add_icon.png'),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TaskPage(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF7349FE),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Image(
+                      image: AssetImage('assets/images/add_icon.png'),
+                    ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
---
+++ lib/screens/taskpage.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class TaskPage extends StatefulWidget {
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 24.0,
+                  bottom: 6.0,
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Image(
+                          image:
+                              AssetImage('assets/images/back_arrow_icon.png'),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Task Title",
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF211551),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Descriptio for the task....",
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 24.0)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
