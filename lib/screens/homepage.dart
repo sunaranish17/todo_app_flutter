6cb955f243f5b09c0bef27072fa42779c82f36ad
Anish <sunaranish17@gmail.com>
2021-08-16 15:10:52 +0545

    Feat: Homepage completed

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -18,13 +18,41 @@ class _HomePageState extends State<HomePage> {
             vertical: 32.0,
           ),
           color: Color(0xFFF6F6F6),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
             children: [
-              Image(
-                image: AssetImage('assets/images/logo.png'),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      bottom: 32.0,
+                    ),
+                    child: Image(
+                      image: AssetImage('assets/images/logo.png'),
+                    ),
+                  ),
+                  TaskCardWidget(
+                      title: "Get Started",
+                      desc:
+                          "Hello User! Welcome to TODO app, this is a default task that you can edit or delete to start using the app."),
+                  TaskCardWidget(),
+                ],
               ),
-              TaskCardWidget(),
+              Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  width: 60.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF7349FE),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Image(
+                    image: AssetImage('assets/images/add_icon.png'),
+                  ),
+                ),
+              )
             ],
           ),
         ),
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
 class TaskCardWidget extends StatelessWidget {
+  final String? title;
+  final String? desc;
+
+  TaskCardWidget({this.title, this.desc});
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       padding: EdgeInsets.symmetric(
         horizontal: 24.0,
         vertical: 32.0,
+      ),
+      margin: EdgeInsets.only(
+        bottom: 20.0,
       ),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20.0),
       ),
-      child: Text("Hello World"),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title ?? "(Unnamed Task)",
+            style: TextStyle(
+                color: Color(0xFF211551),
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 10.0,
+            ),
+            child: Text(
+              desc ?? "No Description Added",
+              style: TextStyle(
+                color: Color(0xFF86829D),
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                height: 1.5,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
